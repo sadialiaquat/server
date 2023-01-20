dd31a688d6811cca991fc565a74be02d09fb7eab
Sadia Liaquat <sadialiaquat988@gmail.com>
2023-01-20 17:42:40 +0500

    message

--- pages/api/refund.js
+++ pages/api/refund.js
@@ -1,6 +1,13 @@
+import NextCors from 'nextjs-cors';
 const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST)
 
 export default async function handler(req, res){
+	await NextCors(req, res, {
+		// Options
+		methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+		origin: '*',
+		optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+	 });
     let { paymentId } = req.body
 	try {
 		await stripe.refunds.create({
